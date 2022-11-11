21f9ca2ef0a1edb2a6c637d3c49738c6360389a7
atif-hanif <94978139+atif-hanif@users.noreply.github.com>
2022-11-11 20:38:43 +0500

    new changes (11-11-2022)

--- js/mad.app.js
+++ js/mad.app.js
@@ -2886,28 +2886,17 @@ var Mad = (function ($) {
 
 })(window.jQuery);
 
-/*
-Video Play
-*/
-$(document).on('click', '.js-videoPoster', function (e) {
-    //отменяем стандартное действие button
-    e.preventDefault();
-    var poster = $(this);
-    // ищем родителя ближайшего по классу
-    var wrapper = poster.closest('.js-videoWrapper');
-    videoPlay(wrapper);
-});
+// Playlist
 
-//вопроизводим видео, при этом скрывая постер
-function videoPlay(wrapper) {
-    var iframe = wrapper.find('.js-videoIframe');
-    // Берем ссылку видео из data
-    var src = iframe.data('src');
-    // скрываем постер
-    wrapper.addClass('videoWrapperActive');
-    // подставляем в src параметр из data
-    iframe.attr('src', src);
-}
+$(document).ready(function () {
+    $('.vid-item').each(function(index){
+        $(this).on('click', function(){
+            var current_index = index+1;
+            $('.vid-item .thumb').removeClass('active');
+            $('.vid-item:nth-child('+current_index+') .thumb').addClass('active');
+        });
+    });
+});
 
 /*
 Plugin Name: 	SVG for jQuery.
